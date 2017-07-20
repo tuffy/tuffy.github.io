1150726363b0aad5777901986821db4e822bbc92
Brian Langenberger <bjl@usa.net>
2017-07-20 12:08:36 -0500

    Attempt to add Matroska documentation

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["matroska"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"matroska/enum.Tracktype.html\" title=\"enum matroska::Tracktype\">Tracktype</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["bitstream_io"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"bitstream_io/huffman/enum.HuffmanTreeError.html\" title=\"enum bitstream_io::huffman::HuffmanTreeError\">HuffmanTreeError</a>",];
+implementors["matroska"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"matroska/enum.Tracktype.html\" title=\"enum matroska::Tracktype\">Tracktype</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
